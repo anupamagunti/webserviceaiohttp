caf276cd33791332d57126c8120e8dce7ef5af99
Anupama Gunti <anupamadatta.g@gmail.com>
2021-11-19 22:48:19 -0600

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,5 +1,6 @@
 import os
 import aiohttp
+import requests
 
 from aiohttp import web
 
@@ -7,6 +8,12 @@ from aiohttp import web
 #from gidgethub import aiohttp as gh_aiohttp
 
 routes = web.RouteTableDef()
+
+base_bitbucket_url = os.environ['BITBUCKETURL']
+bitbucket_api_token = os.environ['BITBUCKETAPITOKEN']
+base_bitbucket_api_url = f'{base_bitbucket_url}/rest/api/1.0'
+#base_bitbucket_api_url = f'{base_bitbucket_url}'
+bitbucket_headers = {"Authorization": f"Bearer {bitbucket_api_token}", "X-Atlassian-Token": "no-check"}
 
 #router = routing.Router()
 
@@ -38,17 +45,65 @@ routes = web.RouteTableDef()
 
 @routes.post("/")
 async def main(request):
-    body = await request.read()
+    body = await request.text()
 
     secret = os.environ.get("GH_SECRET")
     oauth_token = os.environ.get("GH_AUTH")
 
-    #event = sansio.Event.from_http(request.headers, body, secret=secret)
-    #async with aiohttp.ClientSession() as session:
-    #    gh = gh_aiohttp.GitHubAPI(session, "anupamagunti",
-    #                              oauth_token=oauth_token)
-    #    await router.dispatch(event, gh)
-    print(f'Event = @{body}')
+    data = body
+    print(f'Event = @{data}')
+    bot_slug = requests.get(url=f'{base_bitbucket_url}/plugins/servlet/applinks/whoami', headers=bitbucket_headers).text
+    project = data.get('pullRequest', {}).get('toRef').get('repository').get('project').get('key')
+    repo = data.get('pullRequest', {}).get('toRef').get('repository').get('name')
+    pr_id = data.get('pullRequest', {}).get('id')
+    pr_version = data.get('pullRequest', {}).get('version')
+    author_name = data.get("pullRequest", {}).get("author").get("user").get("name")
+    author_display_name = data.get("pullRequest", {}).get("author").get("user").get("displayName")
+    actor_display_name = data.get('actor', {}).get('displayName')
+    src_ref_id = data.get('pullRequest', {}).get('fromRef').get('id')
+    src_repo_id = data.get('pullRequest', {}).get('fromRef').get('repository').get('id')
+    target_ref_id = data.get('pullRequest', {}).get('toRef').get('id')
+    target_repo_id = data.get('pullRequest', {}).get('toRef').get('repository').get('id')
+    target_branch = data.get('pullRequest', {}).get('toRef').get('displayId')
+    base_pr_url = f'{base_bitbucket_api_url}/projects/{project}/repos/{repo}/pull-requests/{pr_id}'
+    
+    def is_pr_mergable(base_pr_url, headers):
+
+        merge_status_url = f'{base_pr_url}/merge'
+        merge_status = requests.get(merge_status_url, headers=headers).json()
+
+        if merge_status.get('canMerge') is True and not merge_status.get('conflicted') and \
+                merge_status.get('outcome') == 'CLEAN':
+            app.logger.debug('PR is mergable, returning True')
+            return True
+        else:
+            return False
+
+    def approve_pr(base_pr_url, headers, slug):
+
+        approval_url = f'{base_pr_url}/participants/{slug}'
+        json_body = {'status': 'APPROVED'}
+        r = requests.put(url=approval_url, headers=headers, json=json_body)
+
+        return r
+
+    def merge_pr(base_pr_url, pr_version, headers):
+
+        merge_url = f'{base_pr_url}/merge?version={pr_version}'
+        r = requests.post(url=merge_url, headers=headers)
+        return r
+
+    if data.get('eventKey', {}) == 'pr:comment:added':
+
+        comment_text = data.get('comment', {}).get('text')
+        print(f"comment_text = @{comment_text}")
+        if comment_text.strip().casefold() == '/approve'.casefold():
+            r = approve_pr(base_pr_url=base_pr_url, headers=bitbucket_headers, slug=bot_slug)
+            is_mergable = is_pr_mergable(base_pr_url=base_pr_url, headers=bitbucket_headers)
+            if is_mergable:
+                merge_pr(base_pr_url=base_pr_url, pr_version=pr_version, headers=bitbucket_headers)
+                
+   
     return web.Response(status=200)
 
 if __name__ == "__main__":
